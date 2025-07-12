364397bccdb521ba90493f14efa18a627c6ccfac
ahmed badie <ahmed.badie.ghoneim@gmail.com>
2025-07-12 16:28:02 +0300

    call class validation

--- login.php
+++ login.php
@@ -3,13 +3,15 @@
     header("Content-Type: application/json");
     require_once("db.php");
     require_once("login_oop.php");
+    require_once("validation.php");
+
 
     $db = new database();
     $connection=$db->conn();
     $ob=new login($connection);
     $ob->handle_request();
-    $ob->validation();
-    $ob->verify_register();
+    $validate = new validation($connection);
+    $validate->validation_login($ob->setemail(),$ob->setpassword());
 
 
 
--- register.php
+++ register.php
@@ -2,13 +2,16 @@
 
     header("Content-Type: application/json");
     require_once("db.php");
-    require_once("register_oop.php");
+    require_once("register_oop.php");    
+    require_once("validation.php");
+
 
     $db = new database();
     $connection=$db->conn();
     $ob=new register($connection);
     $ob->handle_request();
-    $ob->validation();
+    $validate = new validation($connection);
+    $validate->validation_register($ob->setname(),$ob->setemail(),$ob->setpassword()); 
     $ob->hash();
     $ob->verify_register();
 
