9bde52719812a453d575c9131c125a4e988e81f8
ahmed badie <ahmed.badie.ghoneim@gmail.com>
2025-07-20 16:49:47 +0300

    execute regster page

--- RegisterFacade.php
+++ RegisterFacade.php
@@ -4,30 +4,35 @@
     require_once 'service.php';
     require_once 'db.php';
     require_once 'repository.php';
+    require_once 'controller.php';
 
-    class RegisterFacade {          // use Facede pattern
+    class RegisterFacade {          
         private $regist1;
         private $regist2;
-        private $db;
         private $regist3;
+        private $db;
+        private $regist4;
 
         public function __construct(){
             $this->regist1 = new user();
-            $this->regist2 = new service($this->regist1);
             $this->db = database::obFromDatabase();
-            $this->regist3 = new repository($this->regist1,$this->regist2,$this->db);
+            $this->regist3 = new repository($this->db);
+            $this->regist4 = new service($this->regist3,$this->regist1);
+            $this->regist2 = new controller($this->regist1,$this->regist4);
         }
 
         public function register(){
             $this->regist2->handleRequest();
-            $this->regist3->verifyTheDataRegister();
-            $this->regist3->checkPassword();
-            $this->regist3->syntaxOfEmail();
-            $this->regist3->email_reapet();
-            $this->regist3->hash();
-            $this->regist3->returnMessage();
+            $this->regist4->verifyTheDataRegister();
+            $this->regist4->checkPassword();
+            $this->regist4->syntaxOfEmail();
+            $this->regist4->email_reapet($this->regist1->getEmail());
+            $this->regist4->hash($this->regist1->getPassword());
+            $this->regist2->getmessage();
         }
         
     }
+            $re = new RegisterFacade();
+            $re->register();
 
 ?>
