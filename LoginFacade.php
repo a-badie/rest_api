ab8a1db02234e2419b8028207c97e91d2a0cd0e4
ahmed badie <ahmed.badie.ghoneim@gmail.com>
2025-07-20 16:50:26 +0300

    execute login page

--- LoginFacade.php
+++ LoginFacade.php
@@ -4,28 +4,33 @@
     require_once 'service.php';
     require_once 'db.php';
     require_once 'repository.php';
+    require_once 'controller.php';
 
-    class LoginFacade {         // use Facede pattern
+    class LoginFacade {         
         private $log1;
         private $log2;
-        private $db;
         private $log3;
+        private $db;
+        private $log4;
 
         public function __construct(){
             $this->log1 = new user();
-            $this->log2 = new service($this->log1);
             $this->db = database::obFromDatabase();
-            $this->log3 = new repository($this->log1,$this->log2,$this->db);
+            $this->log3 = new repository($this->db);
+            $this->log4 = new service($this->log3,$this->log1);
+            $this->log2 = new controller($this->log1,$this->log4);
         }
 
         public function login(){
             $this->log2->handleRequest();
-            $this->log3->verifyTheDataLogin();
-            $this->log3->email_exist();
-            $this->log3->getinfo();
-            $this->log3->compare_password();
-            $this->log3->returnInsertTokens();  
+            $this->log4->verifyTheDataLogin();
+            $this->log4->email_exist();
+            $this->log4->getinfo();
+            $this->log4->compare_password();
+            $this->log2->getmessageToken();  
         }
     }
+            $re = new LoginFacade();
+            $re->login();
 
 ?>
